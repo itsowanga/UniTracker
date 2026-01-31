d35ecd767e8def264ac600ebab24ab8d9510a83c
itsowanga <sowanga.mbane@gmail.com>
2026-01-31 13:52:22 +0200

    refactor: Remove obsolete HTML files and enhance module management functionality

--- models/module.js
+++ models/module.js
@@ -6,15 +6,69 @@ class Module{
     semester;
     lecturer;
     color;
+    status;
 
-    constructor(id, code, name, semester, lecturer, color){
+    constructor(id, code, name, semester, lecturer, color, status){
         this.id = id;
         this.code = code;
         this.name = name;
         this.semester = semester;
         this.lecturer = lecturer;
         this.color = color;
-
+        this.status = status
+    }
+    get id(){
+        return this.id;
+    }
+    get code(){
+        return this.code;
+    }
+    get name(){
+        return this.name;
+    }
+    get semester(){
+        return this.semester;
+    }
+    get lecturer(){
+        return this.lecturer;
+    }
+    get color(){
+        return this.color;
+    }
+    get status(){
+        return this.status;
+    }
+    set id(id){
+        this.id = id;
+    }
+    set code(code){
+        this.code = code;
+    }
+    set name(name){
+        this.name = name;
+    }
+    set semester(semester){
+        this.semester = semester;
+    }
+    set lecturer(lecturer){
+        this.lecturer = lecturer;
+    }
+    set color(color){
+        this.color = color;
+    }
+    set status(status){
+        this.status = status;
+    }   
+    getJson(){
+        return {
+            id: this.id,
+            code: this.code,
+            name: this.name,
+            semester: this.semester,
+            lecturer: this.lecturer,
+            color: this.color,
+            status: this.status
+        }
     }
 
    
--- models/task.js
+++ models/task.js
@@ -16,5 +16,59 @@ class Task{
         this.weight = weight;
         this.status = status;
         
+    } 
+    get id(){
+        return this.id;
     }
+    get moduleId(){
+        return this.moduleId;
+    }
+    get title(){
+        return this.title;
+    }
+    get dueDate(){
+        return this.dueDate;
+    }
+    get type(){
+        return this.type;
+    }
+    get weight(){
+        return this.weight;
+    }
+    get status(){
+        return this.status;
+    }
+    set id(id){
+        this.id = id;
+    }
+    set moduleId(moduleId){
+        this.moduleId = moduleId;
+    }
+    set title(title){
+        this.title = title;
+    }
+    set dueDate(dueDate){
+        this.dueDate = dueDate;
+    }
+    set type(type){
+        this.type = type;
+    }
+    set weight(weight){
+        this.weight = weight;
+    }
+    set status(status){
+        this.status = status;
+    }
+    getJson(){
+        return {
+            id: this.id,
+            moduleId: this.moduleId,
+            title: this.title,
+            dueDate: this.dueDate,
+            type: this.type,
+            weight: this.weight,
+            status: this.status
+        }
+    }
+    
 }
